9b47f282d445d531b3165f64e13907b815da6491
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2022-09-18 11:14:57 +0800

    Ising_Triangle_bad

--- test/admps.jl
+++ test/admps.jl
@@ -3,7 +3,20 @@ using Ising_Triangle:model_tensor
 using Random
 using Test
 
-@testset "optimize mps" for atype in [Array]
+@testset "optimize mps Ising_Triangle_bad with $atype $updown updown" for updown = [false,true], atype = [Array]
+    Random.seed!(100)
+    β = 1
+    model = Ising_Triangle_bad(1, 1, β)
+    M = reshape(atype(model_tensor(model, Val(:bulk))),4,4,4,4)
+    Au, Ad = optimisemps(M; χ = 20, mapsteps = 20,
+            infolder = "./example/data/$model/", 
+            outfolder = "./example/data/$model/",   
+            verbose= true, updown = updown)
+    env = ADMPS.obs_env(M,Au,Ad)
+    @show ADMPS.Z(env)
+end
+
+@testset "optimize mps Ising_Triangle_good with $atype $updown updown" for updown = [false,true], atype = [Array]
     Random.seed!(100)
     β = 1
     model = Ising_Triangle_good(1, 1, β)
@@ -11,7 +24,7 @@ using Test
     Au, Ad = optimisemps(M; χ = 20, mapsteps = 20,
             infolder = "./example/data/$model/", 
             outfolder = "./example/data/$model/",   
-            verbose= true, updown = true)
+            verbose= true, updown = updown)
     env = ADMPS.obs_env(M,Au,Ad)
     @show ADMPS.Z(env)
 end
