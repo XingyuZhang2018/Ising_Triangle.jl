8ffb4a707aab5aa5bf6c5e4e2e45db2b0092a0f1
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2023-12-20 16:47:57 +0800

    MPSO

--- test/admps.jl
+++ test/admps.jl
@@ -8,27 +8,21 @@ using Random
 using Test
 using Optim
 
-@testset "normality" begin
-    A = rand(ComplexF64, 5, 5)
-    A = A + A'
-
-    val, vec = eigen(A)
-    @test val[1] * vec[:,1] ≈ A * vec[:,1]
-    @test A * vec ≈ vec * diagm(val)
-    @show opnorm(vec, 2) * opnorm(vec^-1, 2)
-    
-end
-
 @testset "optimize mps Ising_Triangle_bad with $atype $updown updown" for updown = [false], atype = [Array]
     Random.seed!(100)
     β = 100
     model = Ising_Triangle_bad(1, 1, β)
     M = reshape(atype(model_tensor(model, Val(:bulk))),4,4,4,4)
-    Au, Ad = optimisemps(M; χ = 20, mapsteps = 20,
-            infolder = "./example/data/$model/", 
-            outfolder = "./example/data/$model/",   
-            verbose= true, updown = updown)
-    env = ADMPS.obs_env(M,Au,Ad)
+    params = ADMPS.Params(D=4, χ1=5, χ2=2, 
+                          opiter=100, 
+                          f_tol = 1e-10,
+                          mapsteps = 20,
+                          infolder="./data/$(model)/", 
+                          outfolder="./data/$(model)/", 
+                          updown=true,
+                          downfromup=false)
+    mps_u, mps_d = optimisemps(M, params)
+    env = ADMPS.obs_env(M, mps_u, mps_d, params)
     @show ADMPS.Z(env)
 end
 
@@ -37,11 +31,16 @@ end
     β = 1
     model = Ising_Triangle_good(1, 1, β)
     M = reshape(atype(model_tensor(model, Val(:bulk))),4,4,4,4)
-    Au, Ad = optimisemps(M; χ = 20, mapsteps = 20,
-            infolder = "./example/data/$model/", 
-            outfolder = "./example/data/$model/",   
-            verbose= true, updown = updown)
-    env = ADMPS.obs_env(M,Au,Ad)
+    params = ADMPS.Params(D=4, χ1=5, χ2=2, 
+                          opiter=100, 
+                          f_tol = 1e-10,
+                          mapsteps = 20,
+                          infolder="./data/$(model)/", 
+                          outfolder="./data/$(model)/", 
+                          updown=true,
+                          downfromup=false)
+    mps_u, mps_d = optimisemps(M, params)
+    env = ADMPS.obs_env(M, mps_u, mps_d, params)
     @show ADMPS.Z(env)
 end
 
@@ -50,12 +49,17 @@ end
     β = 100
     model = Ising_Triangle_bad2(1, 1, β)
     M = reshape(atype(model_tensor(model, Val(:Sbulk))),2,2,2,2)
-    Au, Ad = optimisemps(M; χ = 16, mapsteps = 10, f_tol = 1e-6,
-                         infolder = "./example/data/$model/", 
-                         outfolder = "./example/data/$model/",   
-                         verbose= true, updown = updown
-                         )
-    env = ADMPS.obs_env(M,Au,Ad)
+    params = ADMPS.Params(D=2, χ1=20, χ2=2, 
+                          atype=atype,
+                          opiter=100, 
+                          f_tol = 1e-15,
+                          mapsteps = 10,
+                          infolder="./data/$(model)/", 
+                          outfolder="./data/$(model)/", 
+                          updown=updown,
+                          downfromup=false)
+    mps_u, mps_d = optimisemps(M, params)
+    env = ADMPS.obs_env(M, mps_u, mps_d, params)
     @show norm(log(ADMPS.Z(env)) - 0.3230659669)
 end
 
@@ -64,11 +68,17 @@ end
     β = 100
     model = Ising_Triangle_good(1, 1, β)
     M = reshape(atype(model_tensor(model, Val(:Sbulk))),4,4,4,4)
-    Au, Ad = optimisemps(M; χ = 128, mapsteps = 20, f_tol = 1e-6,
-            infolder = "./example/data/$model/", 
-            outfolder = "./example/data/$model/",   
-            verbose= true, updown = updown)
-    env = ADMPS.obs_env(M,Au,Ad)
+    params = ADMPS.Params(D=4, χ1=20, χ2=4, 
+                          atype=atype,
+                          opiter=20, 
+                          f_tol = 1e-6,
+                          mapsteps = 20,
+                          infolder="./data/$(model)/", 
+                          outfolder="./data/$(model)/", 
+                          updown=true,
+                          downfromup=true)
+    mps_u, mps_d = optimisemps(M, params)
+    env = ADMPS.obs_env(M, mps_u, mps_d, params)
     @show norm(log(ADMPS.Z(env)) - 0.3230659669)
 end
 
@@ -78,11 +88,27 @@ end
     β = 1.2
     model = J1J2_2(1, 1, J2, β)
     M = reshape(atype(model_tensor(model, Val(:bulk))),4,4,4,4)
-    Au, Ad = optimisemps(M; χ = 16, mapsteps = 20,
-            infolder = "./example/data/$model/", 
-            outfolder = "./example/data/$model/",   
-            verbose= true, updown = updown)
-    env = ADMPS.obs_env(M,Au,Ad)
+    params = ADMPS.Params(D=4, χ1=10, χ2=2, 
+                        opiter=20, 
+                        f_tol = 1e-6,
+                        mapsteps = 20,
+                        infolder="./data/$(model)/", 
+                        outfolder="./data/$(model)/", 
+                        updown=true,
+                        downfromup=true)
+    mps_u, mps_d = optimisemps(M, params)
+    env = ADMPS.obs_env(M, mps_u, mps_d, params)
     # @show ADMPS.Z(env)
     @show observable(env, model, Val(:energy)   )
+end
+
+@testset "normality" begin
+    A = rand(ComplexF64, 5, 5)
+    A = A + A'
+
+    val, vec = eigen(A)
+    @test val[1] * vec[:,1] ≈ A * vec[:,1]
+    @test A * vec ≈ vec * diagm(val)
+    @show opnorm(vec, 2) * opnorm(vec^-1, 2)
+    
 end
